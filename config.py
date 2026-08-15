9c9865eed21801bdc95ab9ec96c2ce5c4e4a9821
P2SAMAPA <pranabpattanaik@gmail.com>
2026-08-15 16:23:56 +0800

    Update config.py

--- config.py
+++ config.py
@@ -34,7 +34,7 @@ HF_TOKEN       = os.environ.get("HF_TOKEN", None)
 
 # ── Universes ─────────────────────────────────────────────────────────────────
 EQUITY_SECTORS_TICKERS = [
-    "SPY", "QQQ", "XLK", "XLF", "XLE", "XLV", "XLB", "URA",
+    "SPY", "QQQ", "XLK", "XLF", "XLE", "XLV", "XLB", "URA", "VUG", "VTV", "SPYG", "IWR", "VO", "VB", "VIG", "VEA", "VGT", "VDE", "XLC", "IBB",
     "XLI", "XLY", "XLP", "XLU", "GDX", "XME", "SMH", "SOXX", "IWD", "IWO",
     "IWF", "XSD", "XBI", "IWM",
 ]
